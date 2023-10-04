ad5f6c67b02ae978b665f797989b56d6ba4b5908
Jorge Saldana <jojonoob134@gmail.com>
2023-10-04 11:47:07 -0500

    did view product

--- Testing/Models/IProductRepository.cs
+++ Testing/Models/IProductRepository.cs
@@ -6,5 +6,7 @@ namespace Testing.Models
     public interface IProductRepository
     {
         public IEnumerable<Product> GetAllProducts();
+
+        public Product GetProduct (int id);
     }
 }
